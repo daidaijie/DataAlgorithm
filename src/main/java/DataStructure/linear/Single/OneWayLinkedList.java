9f7e8ab5d031d478e2ea5a0e9fe1726e0339ee99
weiyeli <liweiye1997@gmail.com>
2017-11-15 14:42:20 +0800

    Initial commit
    Signed-off-by: weiyeli <liweiye1997@gmail.com>

--- src/main/java/DataStructure/linear/Single/OneWayLinkedList.java
+++ src/main/java/DataStructure/linear/Single/OneWayLinkedList.java
@@ -4,5 +4,7 @@ package DataStructure.linear.Single;
  * 单向链表
  */
 public class OneWayLinkedList {
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
