c09b4c2484bf21c79a1309e6c4130cf61d80f160
weiyeli <liweiye1997@gmail.com>
2017-11-29 10:46:35 +0800

    更新时间复杂度

---
+++ src/main/java/com/weiyeli/JianZhiOffer/PrintListFromTailToHead.java
@@ -0,0 +1,42 @@
+package com.weiyeli.JianZhiOffer;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * 题目：从尾到头打印链表
+ */
+public class PrintListFromTailToHead {
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 使用栈
+     * @param listNode
+     * @return
+     */
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<ListNode> stack = new Stack<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        ListNode t = listNode;
+        while (t != null) {
+            stack.push(t);
+            t = t.next;
+        }
+
+        while (!stack.isEmpty()) {
+            ListNode listNode1 = stack.pop();
+            arrayList.add(listNode1.val);
+        }
+
+        return arrayList;
+    }
+}
