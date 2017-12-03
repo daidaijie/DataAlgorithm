59a02e670f7524ddeee2099c937da23df5020162
weiyeli <liweiye1997@gmail.com>
2017-12-04 05:40:23 +0800

    剑指offer 旋转数组

---
+++ src/main/java/com/weiyeli/JianZhiOffer/MinNumberInRotateArray.java
@@ -0,0 +1,64 @@
+package com.weiyeli.JianZhiOffer;
+
+
+/**
+ * 旋转数组的最小数字
+ */
+public class MinNumberInRotateArray {
+
+    public static int minNumberInRotateArray(int[] array) {
+
+        if (array == null || array.length == 0)
+            throw new RuntimeException("参数错误");
+
+        if (array.length == 1)
+            return array[0];
+
+        int left = 0;
+        int right = array.length - 1;
+        int mid = left;
+
+        //如果第一个元素小于最后一个元素，说明没有旋转，则直接返回第一个元素
+        while (array[left] >= array[right]) {
+
+            if (right - left == 1) {
+                mid = right;
+                break;
+            }
+
+            mid = left + (right - left) / 2;
+
+            //如果array[left]、array[right]、array[mid]三个相等,则只能顺序查找
+            if (array[left] == array[right] && array[mid] == array[left])
+                return MinInOrder(array);
+
+            if (array[mid] >= array[left]) {
+                left = mid;
+            } else if (array[mid] <= array[right]) {
+                right = mid;
+            }
+        }
+
+        return array[mid];
+    }
+
+    public static int MinInOrder(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i + 1] < array[i])
+                return array[i + 1];
+        }
+        return array[0];
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 0, 1, 1, 1};
+        System.out.println(minNumberInRotateArray(a));
+        int[] b = {1, 1, 1, 0, 1};
+        System.out.println(minNumberInRotateArray(b));
+        int[] c = {1};
+        System.out.println(minNumberInRotateArray(c));
+    }
+
+
+}
---
+++ src/main/java/com/weiyeli/JianZhiOffer/TwoStackToList.java
@@ -0,0 +1,57 @@
+package com.weiyeli.JianZhiOffer;
+
+import java.util.Stack;
+
+/**
+ * 用两个栈实现队列
+ */
+public class TwoStackToList {
+
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    /**
+     * stack1相当于一个缓冲区，用来保存输入的数据
+     * @param code
+     */
+    public void push(int code) {
+        stack1.push(code);
+    }
+
+    /**
+     * stack2用来负责输出，因为队列是先进先出，所以栈1中栈底的在栈2中是栈顶
+     * @return
+     */
+    public int pop() {
+        if (stack1.empty() && stack2.empty()) {
+            throw new RuntimeException("Queue is empty!");
+        }
+
+        /**
+         * 当同一个批次的输出完后，在从1中读取数据
+         */
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        return stack2.pop();
+    }
+
+
+    public static void main(String[] args) {
+        TwoStackToList list = new TwoStackToList();
+        list.push(1);
+//        list.push(2);
+//        list.push(3);
+//        list.push(4);
+//        list.push(5);
+
+//        System.out.println(list.pop());
+//        System.out.println(list.pop());
+//        System.out.println(list.pop());
+//        System.out.println(list.pop());
+        System.out.println(list.pop());
+    }
+}
