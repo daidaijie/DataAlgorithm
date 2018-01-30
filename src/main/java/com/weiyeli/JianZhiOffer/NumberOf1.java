cfc490509a391f1e540245210a3185fd80497ca7
weiyeli <liweiye1997@gmail.com>
2018-01-30 11:49:27 +0800

    add BinaryTree and some new files

--- src/main/java/com/weiyeli/JianZhiOffer/NumberOf1.java
+++ src/main/java/com/weiyeli/JianZhiOffer/NumberOf1.java
@@ -5,16 +5,23 @@ package com.weiyeli.JianZhiOffer;
  */
 public class NumberOf1 {
 
+    /**
+     * 思路：把一个整数减去1，将这个数与原来的数做与运算，会将原来那个数的最后一个1变成0
+     * 所以最终这个数会变成0，能做几次这样的操作，就说明有几个1
+     * @param n
+     * @return
+     */
     public static int NumberOf1(int n) {
         int count = 0;
         while (n != 0) {
-            n = n & (n-1);
+            n = n & (n - 1);
             count++;
         }
         return count;
     }
 
     public static void main(String[] args) {
+        
         System.out.println(NumberOf1(-9));
     }
 }
--- src/main/java/com/weiyeli/JianZhiOffer/Power.java
+++ src/main/java/com/weiyeli/JianZhiOffer/Power.java
@@ -4,19 +4,21 @@ package com.weiyeli.JianZhiOffer;
  * 数值的整数次方
  */
 public class Power {
-    public static double Power(double base, int exponent) {
+    public static double Power(double base, int exponent) throws IllegalArgumentException {
+        // 如果指数为0返回1
         if (exponent == 0)
             return 1.0;
         else if (exponent > 0) {
-            return ZPower(base, exponent);
+            return PowerWithUnsignedExponent(base, exponent);
         }
         else {
             //判断base是不是0
-            if (base == 0) {
-                return 0;
+            if (equal(base, 0.0)) {
+                //应该抛出一个异常
+                throw  new IllegalArgumentException();
             }else {
                int zexponent = Math.abs(exponent);
-               return 1.0 / ZPower(base, zexponent);
+               return 1.0 / PowerWithUnsignedExponent(base, zexponent);
             }
         }
     }
@@ -29,7 +31,32 @@ public class Power {
         return sum;
     }
 
+    //double不能直接判断
+    private static boolean equal(double num1, double num2) {
+        if ((num1 - num2 > -0.0000001) && (num1-num2 < 0.0000001))
+            return true;
+        else return false;
+    }
+
+    //通过递归优化我们的算法，而不是一味的做乘法
+    private static double PowerWithUnsignedExponent(double base, int exponent) {
+        if (exponent == 0)
+            return 1;
+        if (exponent == 1)
+            return base;
+
+        double result = PowerWithUnsignedExponent(base, exponent >> 1);
+        result *= result;
+        //如果是奇数，则还需要乘一次base
+        if ((exponent & 0x1) == 1) {
+            result *= base;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Power(0, 0));
+
+        System.out.println(Power(2,12));
     }
 }
