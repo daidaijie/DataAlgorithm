3a94ed3f30a72fb32cfe1fd2670790ad19b31375
weiyeli <liweiye1997@gmail.com>
2018-03-30 15:00:37 +0800

    更新了美团笔试题
    增加了动态规划方法

--- src/main/java/com/weiyeli/Nowcoder/Meituan/Main2.java
+++ src/main/java/com/weiyeli/Nowcoder/Meituan/Main2.java
@@ -1,7 +1,5 @@
 package com.weiyeli.Nowcoder.Meituan;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main2 {
@@ -10,8 +8,10 @@ public class Main2 {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] a = {1, 5, 10, 20, 50, 100};
-//        System.out.println(dp(a, 0, n));
-        System.out.println(coins2(a, n));
+        System.out.println("暴力搜索法: " + dp(a, 0, n));
+        System.out.println("记忆搜索法: " + coins2(a, n));
+        System.out.println("动态规划法: " + coins3(a, n));
+        System.out.println("一维动态规划法: " + coins4(a,n));
     }
 
     /**
@@ -74,5 +74,63 @@ public class Main2 {
         }
         dp[index][aim] = res;
         return res;
+    }
+
+    /**
+     * 动态规划方法
+     * dp[i][j]的含义是在使用arr[0..i]种货币的情况下，组成钱数j有多少方法
+     *
+     * @param arr
+     * @param aim
+     * @return
+     */
+    public static long coins3(int[] arr, int aim) {
+        long[][] dp = new long[arr.length][aim + 1];
+        // 初始状态
+        for (int i = 0; i < dp.length; i++)
+            dp[i][0] = 1;
+        for (int i = 0; i <= aim; i++) {
+            //第一行就是只能用arr[0]这种货币，aim只能是arr[0]的整数倍
+            int n = arr[0];
+            if (i % n == 0) dp[0][i] = 1;
+            else dp[0][i] = 0;
+        }
+
+        //从上到下，从左到右
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j <= aim; j++) {
+                // 判断总的目标数是否大于当前的钱的面值
+                // 如果小于，则在前i-1项里面拼凑j
+                if (arr[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - arr[i]];
+                }
+            }
+        }
+
+        return dp[arr.length - 1][aim];
+    }
+
+    /**
+     * 一维数组动态规划方法
+     */
+    public static long coins4(int[] arr, int aim) {
+        // 表示每种钱数j有多少种拼凑方法
+        long[] dp = new long[aim + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= aim; i++) {
+            if (i % arr[0] == 0) dp[i] = 1;
+            else dp[i] = 0;
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j <= aim; j++) {
+                if (j >= arr[i])
+                    dp[j] = dp[j] + dp[j-arr[i]];
+            }
+        }
+
+        return dp[aim];
     }
 }
